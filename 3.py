12106dde1fafd28dd64f6fe686be198a5f01e576
[J] <[jasonjameson89@hotmail.co.uk]>
2021-09-14 10:29:40 +0100

    adasdasdasda

--- 3.py
+++ 3.py
@@ -1,17 +1,15 @@
 bicycles = ['trek','kona','anythingelse']
 print(bicycles)
 
-print(bicycles[0-1])
+print(bicycles[0])
+print(bicycles[1])
 print(bicycles[2])
-print(bicycles[0-2])
 
 
-
-
-
-
-
-
+cars = ['honda','vw','bmw']
+print(cars)
+print(cars[0])
+print(cars[0].title())
 
 
 
