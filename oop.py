56478898ad8cffa821f0e6370c9e59bac9d9d487
[J] <[jasonjameson89@hotmail.co.uk]>
2021-09-17 10:05:18 +0100

    sdf

--- oop.py
+++ oop.py
@@ -1,17 +1,30 @@
-oop
+#oop
 #class
-class my class:
+class my_class:
     x = 5
 
 #object
-object = myclass()
+object = my_class()
 print(object.x)
 
 #^^simplest form
 
 #below is true example
-#__init__ function
+#__init__ function <- on switch
 
+
+class person:
+    def __init__(self,x,y,z):
+        self.x = x
+        self.y = y 
+        self.z = z
+
+p1 = person('j','32','male')
+
+print(p1.)
+print(p1.x)
+print(p1.y)
+print(p1.z)
 
 
 
