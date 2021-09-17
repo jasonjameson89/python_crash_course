7dc0d5d22cafd3e31bf4c3913e3309f0e43e4e81
[J] <[jasonjameson89@hotmail.co.uk]>
2021-09-17 10:18:01 +0100

    asd

---
+++ pyhtonCRASHcourse.py
@@ -0,0 +1,47 @@
+#Variables and data types:
+
+print('hello world')
+
+message = 'hello world message'
+print(message)
+
+
+#Strings
+
+x = 'this is strings text'
+y = 'thisisstringtext'
+z = 'this is also 123 string text'
+xyz = '123123'
+print(x,y,z,xyz)
+
+
+#String manipulation
+
+#lowercase
+#uppercase
+x = 'Message'
+print(x.lower())
+print(x.upper())
+
+#Using variables in strings
+
+x = 'jason'
+y = 'jameson'
+print('hello ' + x + ' ' + y + '.' )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
